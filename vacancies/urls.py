445ebce12230998f4e0d8432546a16b6f07737de
MrZed-png <balakirevdanil2233@mail.ru>
2023-03-10 16:24:28 +0300

    annotate+aggregate vacancies

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('<int:pk>', views.VacancyDetailView.as_view()),
     path('create/', views.VacancyCreateView.as_view()),
     path('<int:pk>/update/', views.VacancyUpdateView.as_view()),
-    path('<int:pk>/delete/', views.VacancyDeleteView.as_view())
+    path('<int:pk>/delete/', views.VacancyDeleteView.as_view()),
+    path('by_user/', views.UserVacancyDetailView.as_view())
 ]
--- vacancies/views.py
+++ vacancies/views.py
@@ -1,6 +1,8 @@
 import json
 
+from django.contrib.auth.models import User
 from django.core.paginator import Paginator
+from django.db.models import Count, Avg
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
@@ -127,3 +129,28 @@ class VacancyDeleteView(DeleteView):
         super().delete(request, *args, **kwargs)
 
         return JsonResponse({"status": "ok"}, status=200)
+
+
+class UserVacancyDetailView(View):
+    def get(self, request):
+        user_qs = User.objects.annotate(vacancies=Count('vacancy'))
+
+        paginator = Paginator(user_qs, settings.TOTAL_ON_PAGE)
+        page_number = request.GET.get("page")
+        page_odj = paginator.get_page(page_number)
+
+        users = []
+        for user in page_odj:
+            users.append({
+                "id": user.id,
+                "name": user.username,
+                "vacancies": user.vacancies
+            })
+
+        resource = {
+           "items": users,
+           "total": paginator.count,
+           "num_pages": paginator.num_pages,
+           "avg": user_qs.aggregate(avg=Avg('vacancies'))["avg"]
+        }
+        return JsonResponse(resource)
