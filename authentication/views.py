e871f4b092827cd23e1e4c3956b90acbead82f4e
MrZed-png <balakirevdanil2233@mail.ru>
2023-04-17 14:32:20 +0300

    fix filter auth_token

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from authentication.models import User
 from authentication.serializers import UserCreateSerialize
@@ -8,3 +11,9 @@ from authentication.serializers import UserCreateSerialize
 class UserCreateView(CreateAPIView):
     model = User
     serializer_class = UserCreateSerialize
+
+
+class Logout(APIView):
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
